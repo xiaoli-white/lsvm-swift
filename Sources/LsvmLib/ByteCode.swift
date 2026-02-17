8e84d3589066ef9764653baa5593cb69501a97cd
xiaoli-white <maxljh0216@outlook.com>
2026-02-18 01:54:37 +0800

    ✨ feat(Interpreter): 支持字典构建和原生函数调用

--- Sources/LsvmLib/ByteCode.swift
+++ Sources/LsvmLib/ByteCode.swift
@@ -1,45 +1,46 @@
 public enum ByteCode: UInt8 {
-  case NOP = 0x00
-  case EXTENDED_ARG
-  case LOAD_CONSTANT
-  case PUSH_SMALL_INT
-  case MAKE_FUNCTION
-  case BINARY_OP
-  case COMPARE_OP
-  case POP_TOP
-  case DUP_TOP
-  case CALL
-  case RETURN_VALUE
-  case RETURN_CONSTANT
-  case RETURN_NULL
-  case JUMP_IF_TRUE
-  case JUMP_IF_FALSE
-  case JUMP_ABSOLUTE
-  case JUMP_FORWARD
-  case JUMP_BACKWARD
-  case LOAD_NAME
-  case STORE_NAME
-  case LOAD_LOCAL
-  case STORE_LOCAL
-  case LOAD_GLOBAL
-  case STORE_GLOBAL
-  case LOAD_ATTR
-  case STORE_ATTR
-  case BUILD_LIST
-  public enum BinaryOp: UInt8 {
-    case ADD = 0x00
-    case SUB
-    case MUL
-    case DIV
-    case MOD
-    case POW
-  }
-  public enum CompareOp: UInt8 {
-    case EQUAL = 0x00
-    case NOT_EQUAL
-    case LESS
-    case LESS_EQUAL
-    case GREATER
-    case GREATER_EQUAL
-  }
+    case NOP = 0x00
+    case EXTENDED_ARG
+    case LOAD_CONSTANT
+    case PUSH_SMALL_INT
+    case MAKE_FUNCTION
+    case BINARY_OP
+    case COMPARE_OP
+    case POP_TOP
+    case DUP_TOP
+    case CALL
+    case RETURN_VALUE
+    case RETURN_CONSTANT
+    case RETURN_NULL
+    case JUMP_IF_TRUE
+    case JUMP_IF_FALSE
+    case JUMP_ABSOLUTE
+    case JUMP_FORWARD
+    case JUMP_BACKWARD
+    case LOAD_NAME
+    case STORE_NAME
+    case LOAD_LOCAL
+    case STORE_LOCAL
+    case LOAD_GLOBAL
+    case STORE_GLOBAL
+    case LOAD_ATTR
+    case STORE_ATTR
+    case BUILD_LIST
+    case BUILD_DICT
+    public enum BinaryOp: UInt8 {
+        case ADD = 0x00
+        case SUB
+        case MUL
+        case DIV
+        case MOD
+        case POW
+    }
+    public enum CompareOp: UInt8 {
+        case EQUAL = 0x00
+        case NOT_EQUAL
+        case LESS
+        case LESS_EQUAL
+        case GREATER
+        case GREATER_EQUAL
+    }
 }
--- Sources/LsvmLib/Interpreter.swift
+++ Sources/LsvmLib/Interpreter.swift
@@ -1,239 +1,257 @@
 public final class Interpreter {
-  private var currentFrame: Object.FrameObject? = nil
-  public init() {
+    private var currentFrame: Object.FrameObject? = nil
+    public init() {
 
-  }
-  public func run(code: Object.CodeObject) {
-    let frame = Object.FrameObject(code: code, builtins: Builtins.getBuiltins())
-    evalFrame(frame)
-  }
-  public func evalFrame(_ frame: Object.FrameObject) {
-    currentFrame = frame
-    var arg: UInt32 = 0
-    loop: while true {
-      let op = ByteCode(rawValue: currentFrame!.code.code[currentFrame!.pc])!
-      currentFrame!.pc += 1
-      switch op {
-      case .NOP:
-        break
-      case .EXTENDED_ARG:
-        arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-      case .LOAD_CONSTANT:
-        arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-        push(currentFrame!.code.constants[Object.IntegerObject(value: Int64(arg))]!)
-      case .PUSH_SMALL_INT:
-        arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-        push(Object.IntegerObject(value: Int64(arg)))
-      case .MAKE_FUNCTION:
-        let codeObject = pop() as! Object.CodeObject
-        let function = Object.FunctionObject(
-          code: codeObject, globals: currentFrame!.globals, builtins: Builtins.getBuiltins())
-        push(function)
-      case .BINARY_OP:
-        let op = ByteCode.BinaryOp(rawValue: currentFrame!.code.code[currentFrame!.pc])!
-        currentFrame!.pc += 1
-        let right = pop()
-        let left = top()
-        switch op {
-        case .ADD:
-          setTop(left.add(right))
-        case .SUB:
-          setTop(left.sub(right))
-        case .MUL:
-          setTop(left.mul(right))
-        case .DIV:
-          setTop(left.div(right))
-        case .MOD:
-          setTop(left.mod(right))
-        case .POW:
-          setTop(left.pow(right))
-        }
-      case .COMPARE_OP:
-        let op = ByteCode.CompareOp(rawValue: currentFrame!.code.code[currentFrame!.pc])!
-        currentFrame!.pc += 1
-        let right = pop()
-        let left = top()
-        switch op {
-        case .EQUAL:
-          setTop(left.equal(right))
-        case .NOT_EQUAL:
-          setTop(left.notEqual(right))
-        case .LESS:
-          setTop(left.less(right))
-        case .LESS_EQUAL:
-          setTop(left.lessEqual(right))
-        case .GREATER:
-          setTop(left.greater(right))
-        case .GREATER_EQUAL:
-          setTop(left.greaterEqual(right))
-        }
-      case .POP_TOP:
-        let _ = pop()
-      case .DUP_TOP:
-        push(top())
-      case .CALL:
-        arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-        let args = Object.ListObject()
-        for _ in 0..<Int(arg) {
-          args.append(pop())
-        }
-        let f = pop()
-        if let funcObject = f as? Object.FunctionObject {
-          let frame = Object.FrameObject(
-            code: funcObject.code, builtins: currentFrame!.builtins, parent: currentFrame)
-          currentFrame = frame
-        }
-      case .RETURN_VALUE:
-        let value = pop()
-        if currentFrame!.parent == nil {
-          break loop
-        }
-        currentFrame = currentFrame!.parent
-        push(value)
-      case .RETURN_CONSTANT:
-        arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-        if currentFrame!.parent == nil {
-          break loop
-        }
-        let constant = currentFrame!.code.constants[Object.IntegerObject(value: Int64(arg))]!
-        currentFrame = currentFrame!.parent
-        push(constant)
-      case .RETURN_NULL:
-        if currentFrame!.parent == nil {
-          break loop
-        }
-        currentFrame = currentFrame!.parent
-        push(Object.NullObject.instance)
-      case .JUMP_IF_TRUE:
-        let target = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-        let t = pop() as! Object.BooleanObject
-        if t.value {
-          currentFrame!.pc = Int(target)
-        }
-      case .JUMP_IF_FALSE:
-        let target = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-        let t = pop() as! Object.BooleanObject
-        if !t.value {
-          currentFrame!.pc = Int(target)
-        }
-      case .JUMP_ABSOLUTE:
-        let target = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc = Int(target)
-      case .JUMP_FORWARD:
-        let offset = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += Int(offset) + 1
-      case .JUMP_BACKWARD:
-        let offset = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-        currentFrame!.pc -= Int(offset)
-      case .LOAD_NAME:
-        arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-        let name =
-          currentFrame!.code.names[Object.IntegerObject(value: Int64(arg))]!
-          as! Object.StringObject
-        var value = currentFrame!.locals[name]
-        if value == nil {
-          value = currentFrame!.globals[name]
-        }
-        if value == nil {
-          value = currentFrame!.builtins[name]
-        }
-        push(value!)
-      case .STORE_NAME:
-        arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-        let name =
-          currentFrame!.code.names[Object.IntegerObject(value: Int64(arg))]!
-          as! Object.StringObject
-        let value = pop()
-        if currentFrame!.locals.contains(key: name) {
-          currentFrame!.locals[name] = value
-        } else {
-          currentFrame!.globals[name] = value
-        }
-      case .LOAD_LOCAL:
-        arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-        let name =
-          currentFrame!.code.varnames[Object.IntegerObject(value: Int64(arg))]!
-          as! Object.StringObject
-        push(currentFrame!.locals[name]!)
-      case .STORE_LOCAL:
-        arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-        let name =
-          currentFrame!.code.varnames[Object.IntegerObject(value: Int64(arg))]!
-          as! Object.StringObject
-        let value = pop()
-        currentFrame!.locals[name] = value
-      case .LOAD_GLOBAL:
-        arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-        let name =
-          currentFrame!.code.names[Object.IntegerObject(value: Int64(arg))]! as! Object.StringObject
-        push(currentFrame!.globals[name]!)
-      case .STORE_GLOBAL:
-        arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-        let name =
-          currentFrame!.code.names[Object.IntegerObject(value: Int64(arg))]! as! Object.StringObject
-        let value = pop()
-        currentFrame!.globals[name] = value
-      case .BUILD_LIST:
-        arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
-        currentFrame!.pc += 1
-        let list = Object.ListObject()
-        for _ in 0 ..< Int(arg) {
-          list.append(pop())
-        }
-        push(list)
-      default:
-        break
-      }
-      if op != .EXTENDED_ARG {
-        arg = 0
-      }
     }
-    currentFrame = nil
-  }
-  public func stackSize() -> UInt64 {
-    return currentFrame!.code.stackSize
-  }
-  public func stackDepth() -> UInt64 {
-    let stackAddr = UInt64(bitPattern: Int64(UInt(bitPattern: currentFrame!.stack)))
-    let baseAddr = UInt64(bitPattern: Int64(UInt(bitPattern: currentFrame!.stackBase)))
-    return UInt64(
-      (stackAddr - baseAddr) / UInt64(MemoryLayout<UnsafeMutablePointer<Object.BaseObject>>.size))
-  }
-  public func isStackEmpty() -> Bool {
-    return currentFrame!.stack == currentFrame!.stackBase
-  }
-  public func top() -> Object.BaseObject {
-    return currentFrame!.stack[-1]
-  }
-  public func second() -> Object.BaseObject {
-    currentFrame!.stack[-2]
-  }
-  public func setTop(_ x: Object.BaseObject) {
-    currentFrame!.stack[-1] = x
-  }
-  public func setSecond(_ x: Object.BaseObject) {
-    currentFrame!.stack[-2] = x
-  }
-  public func push(_ x: Object.BaseObject) {
-    currentFrame!.stack.pointee = x
-    currentFrame!.stack = currentFrame!.stack.advanced(by: 1)
-  }
-  public func pop() -> Object.BaseObject {
-    currentFrame!.stack = currentFrame!.stack.advanced(by: -1)
-    return currentFrame!.stack.pointee
-  }
+    public func run(code: Object.CodeObject) {
+        let frame = Object.FrameObject(code: code, builtins: Builtins.getBuiltins())
+        evalFrame(frame)
+    }
+    public func evalFrame(_ frame: Object.FrameObject) {
+        currentFrame = frame
+        var arg: UInt32 = 0
+        loop: while true {
+            let op = ByteCode(rawValue: currentFrame!.code.code[currentFrame!.pc])!
+            currentFrame!.pc += 1
+            switch op {
+            case .NOP:
+                break
+            case .EXTENDED_ARG:
+                arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+            case .LOAD_CONSTANT:
+                arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                push(currentFrame!.code.constants[Object.IntegerObject(value: Int64(arg))]!)
+            case .PUSH_SMALL_INT:
+                arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                push(Object.IntegerObject(value: Int64(arg)))
+            case .MAKE_FUNCTION:
+                let codeObject = pop() as! Object.CodeObject
+                let function = Object.FunctionObject(
+                    code: codeObject, globals: currentFrame!.globals,
+                    builtins: Builtins.getBuiltins())
+                push(function)
+            case .BINARY_OP:
+                let op = ByteCode.BinaryOp(rawValue: currentFrame!.code.code[currentFrame!.pc])!
+                currentFrame!.pc += 1
+                let right = pop()
+                let left = top()
+                switch op {
+                case .ADD:
+                    setTop(left.add(right))
+                case .SUB:
+                    setTop(left.sub(right))
+                case .MUL:
+                    setTop(left.mul(right))
+                case .DIV:
+                    setTop(left.div(right))
+                case .MOD:
+                    setTop(left.mod(right))
+                case .POW:
+                    setTop(left.pow(right))
+                }
+            case .COMPARE_OP:
+                let op = ByteCode.CompareOp(rawValue: currentFrame!.code.code[currentFrame!.pc])!
+                currentFrame!.pc += 1
+                let right = pop()
+                let left = top()
+                switch op {
+                case .EQUAL:
+                    setTop(left.equal(right))
+                case .NOT_EQUAL:
+                    setTop(left.notEqual(right))
+                case .LESS:
+                    setTop(left.less(right))
+                case .LESS_EQUAL:
+                    setTop(left.lessEqual(right))
+                case .GREATER:
+                    setTop(left.greater(right))
+                case .GREATER_EQUAL:
+                    setTop(left.greaterEqual(right))
+                }
+            case .POP_TOP:
+                let _ = pop()
+            case .DUP_TOP:
+                push(top())
+            case .CALL:
+                arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                let args = Object.ListObject()
+                for _ in 0..<Int(arg) {
+                    args.append(pop())
+                }
+                let f = pop()
+                if let funcObject = f as? Object.FunctionObject {
+                    let frame = Object.FrameObject(
+                        code: funcObject.code, builtins: currentFrame!.builtins,
+                        parent: currentFrame)
+                    currentFrame = frame
+                } else if let nativeFuncObject = f as? Object.NativeFunctionObject {
+                    push(nativeFuncObject.function(args))
+                }
+            case .RETURN_VALUE:
+                let value = pop()
+                if currentFrame!.parent == nil {
+                    break loop
+                }
+                currentFrame = currentFrame!.parent
+                push(value)
+            case .RETURN_CONSTANT:
+                arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                if currentFrame!.parent == nil {
+                    break loop
+                }
+                let constant = currentFrame!.code.constants[
+                    Object.IntegerObject(value: Int64(arg))]!
+                currentFrame = currentFrame!.parent
+                push(constant)
+            case .RETURN_NULL:
+                if currentFrame!.parent == nil {
+                    break loop
+                }
+                currentFrame = currentFrame!.parent
+                push(Object.NullObject.instance)
+            case .JUMP_IF_TRUE:
+                let target = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                let t = pop() as! Object.BooleanObject
+                if t.value {
+                    currentFrame!.pc = Int(target)
+                }
+            case .JUMP_IF_FALSE:
+                let target = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                let t = pop() as! Object.BooleanObject
+                if !t.value {
+                    currentFrame!.pc = Int(target)
+                }
+            case .JUMP_ABSOLUTE:
+                let target = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc = Int(target)
+            case .JUMP_FORWARD:
+                let offset = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += Int(offset) + 1
+            case .JUMP_BACKWARD:
+                let offset = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                currentFrame!.pc -= Int(offset)
+            case .LOAD_NAME:
+                arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                let name =
+                    currentFrame!.code.names[Object.IntegerObject(value: Int64(arg))]!
+                    as! Object.StringObject
+                var value = currentFrame!.locals[name]
+                if value == nil {
+                    value = currentFrame!.globals[name]
+                }
+                if value == nil {
+                    value = currentFrame!.builtins[name]
+                }
+                push(value!)
+            case .STORE_NAME:
+                arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                let name =
+                    currentFrame!.code.names[Object.IntegerObject(value: Int64(arg))]!
+                    as! Object.StringObject
+                let value = pop()
+                if currentFrame!.locals.contains(key: name) {
+                    currentFrame!.locals[name] = value
+                } else {
+                    currentFrame!.globals[name] = value
+                }
+            case .LOAD_LOCAL:
+                arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                let name =
+                    currentFrame!.code.varnames[Object.IntegerObject(value: Int64(arg))]!
+                    as! Object.StringObject
+                push(currentFrame!.locals[name]!)
+            case .STORE_LOCAL:
+                arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                let name =
+                    currentFrame!.code.varnames[Object.IntegerObject(value: Int64(arg))]!
+                    as! Object.StringObject
+                let value = pop()
+                currentFrame!.locals[name] = value
+            case .LOAD_GLOBAL:
+                arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                let name =
+                    currentFrame!.code.names[Object.IntegerObject(value: Int64(arg))]!
+                    as! Object.StringObject
+                push(currentFrame!.globals[name]!)
+            case .STORE_GLOBAL:
+                arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                let name =
+                    currentFrame!.code.names[Object.IntegerObject(value: Int64(arg))]!
+                    as! Object.StringObject
+                let value = pop()
+                currentFrame!.globals[name] = value
+            case .BUILD_LIST:
+                arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                let list = Object.ListObject()
+                for _ in 0..<Int(arg) {
+                    list.append(pop())
+                }
+                push(list)
+            case .BUILD_DICT:
+                arg = (arg << 8) | UInt32(currentFrame!.code.code[currentFrame!.pc])
+                currentFrame!.pc += 1
+                let dict = Object.DictObject()
+                for _ in 0..<Int(arg) {
+                    let key = pop()
+                    let value = pop()
+                    dict[key] = value
+                }
+                push(dict)
+            default:
+                break
+            }
+            if op != .EXTENDED_ARG {
+                arg = 0
+            }
+        }
+        currentFrame = nil
+    }
+    public func stackSize() -> UInt64 {
+        return currentFrame!.code.stackSize
+    }
+    public func stackDepth() -> UInt64 {
+        let stackAddr = UInt64(bitPattern: Int64(UInt(bitPattern: currentFrame!.stack)))
+        let baseAddr = UInt64(bitPattern: Int64(UInt(bitPattern: currentFrame!.stackBase)))
+        return UInt64(
+            (stackAddr - baseAddr)
+                / UInt64(MemoryLayout<UnsafeMutablePointer<Object.BaseObject>>.size))
+    }
+    public func isStackEmpty() -> Bool {
+        return currentFrame!.stack == currentFrame!.stackBase
+    }
+    public func top() -> Object.BaseObject {
+        return currentFrame!.stack[-1]
+    }
+    public func second() -> Object.BaseObject {
+        currentFrame!.stack[-2]
+    }
+    public func setTop(_ x: Object.BaseObject) {
+        currentFrame!.stack[-1] = x
+    }
+    public func setSecond(_ x: Object.BaseObject) {
+        currentFrame!.stack[-2] = x
+    }
+    public func push(_ x: Object.BaseObject) {
+        currentFrame!.stack.pointee = x
+        currentFrame!.stack = currentFrame!.stack.advanced(by: 1)
+    }
+    public func pop() -> Object.BaseObject {
+        currentFrame!.stack = currentFrame!.stack.advanced(by: -1)
+        return currentFrame!.stack.pointee
+    }
 }
